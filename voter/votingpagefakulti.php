a1cf166ff1b871938c6d8ead3f90cf0bf4e38b25
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-07-14 15:50:11 +0800

    align the coding
    align the coding to easy understand

--- voter/votingpagefakulti.php
+++ voter/votingpagefakulti.php
@@ -4,7 +4,7 @@ session_start();
 if(empty($_SESSION['id'])){
 header("location:../index.php?error=alreadylogout");
 }
-// get voter id & voter faculty  from session
+// get voter id & voter faculty_id  from session
 $voterid=$_SESSION['id'];
 $voterfaculty=$_SESSION['faculty'];
 // print the voter id
@@ -12,11 +12,11 @@ echo "voter id:". $voterid."<br>";
 
 // get selected from previos page
 $_SESSION['umum']=$_POST['umum_candidate_selected'];
-// check selection from previos page
 
+// check selection from previos page
 if (empty($_SESSION['umum'])) {
   // if selection empty return to previos page
-header("Location:votingpageumum.php?error=selection_empty");
+   header("Location:votingpageumum.php?error=selection_empty");
 }
 
 $pilihanumum=$_SESSION['umum'];
@@ -52,44 +52,41 @@ include 'include/header_votingpage.php';
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
-                <div class="p-5">
-
-                  <!-- form start -->
-                  <form name="form_fakulti" method="POST" action="">
-                    <table class="table ">
-                      <thead>
-                        <h2>calon fakulti</h2>
-                        <div class="alert alert-info" role="alert">Please select (amount) candidate!</div>
-
-                         <tr>
-                            <th scope="col">Candidate ID</th>
-                            <th scope="col">Candidate Name</th>
-                            <th scope="col">Action</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                        <?php
-                          while ($record=mysqli_fetch_array($qr)){//redo to other records
-                        ?>
+            <div class="p-5">
+              <!-- form start -->
+              <form name="form_fakulti" method="POST" action="confirmation_votingpage.php">
+                <table class="table ">
+                  <thead>
+                    <h2>calon fakulti</h2>
+                    <div class="alert alert-info" role="alert">Please select (amount) candidate!</div>
                       <tr>
-                        <td ><?=$record['candidate_id']?></td>
-                        <td><?=$record['voter_name']?></td>
-                        <td>
-                          <div class="form-check">
-                            <input name="fakulti_candidate_selected" class="form-check-input" type="checkbox" value="" >
-                          </div>
-                        </td>
+                        <th scope="col">Candidate ID</th>
+                        <th scope="col">Candidate Name</th>
+                        <th scope="col">Action</th>
                       </tr>
-                      <?php
-                        }//end of records
-                      ?>   
-                      </tbody>
-                    </table>
-                    <input class="btn btn-danger" name="btn_back" type="button" value="Back" onclick="history.back()">
-                    <input class="btn btn-primary" type="submit" name="btn_submit_vote" value="Submit">
-
-                  </form>
-                </div>
+                  </thead>
+                  <tbody>
+                    <?php
+                      while ($record=mysqli_fetch_array($qr)){//redo to other records
+                    ?>
+                  <tr>
+                    <td ><?=$record['candidate_id']?></td>
+                    <td><?=$record['voter_name']?></td>
+                    <td>
+                      <div class="form-check">
+                        <input name="fakulti_candidate_selected[]" class="form-check-input" type="checkbox" value="<?=$record['candidate_id']?>" >
+                      </div>
+                    </td>
+                  </tr>
+                  <?php
+                    }//end of records
+                  ?>   
+                  </tbody>
+                </table>
+                <input class="btn btn-danger" name="btn_back" type="button" value="Back" onclick="history.back()">
+                <input class="btn btn-primary" type="submit" name="btn_submit_vote" value="Submit">
+              </form>
+            </div>
           </div>
         </div>
 
