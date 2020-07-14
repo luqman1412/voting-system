e1a7bba81fb239d7e67d16be574427b4378b42dc
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-07-14 20:53:00 +0800

    susun semula coding
    cuma susun coding

--- voter/confirmation_votingpage.php
+++ voter/confirmation_votingpage.php
@@ -23,7 +23,7 @@ include 'include/header_votingpage.php'
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-7 col-lg-12 col-md-9">
+      <div class="col-xl-9 col-lg-12 col-md-9">
         <!-- card element -->
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-header py-3" >
@@ -31,79 +31,84 @@ include 'include/header_votingpage.php'
           </div>
           <div class="card-body p-0">
             <div class="p-5">
+              <div class="row">
+                <div class="col">
                   <h3>candidate umum</h3>
+                    <form method="POST" action="insertvotingintoDB.php">
+                      <table class="table table-bordered">
+                        <tr>
+                          <th class="th-sm" >Candidate ID</th>
+                          <th class="th-sm" >Name</th>
+                          <th class="th-sm" >Action</th>
+                        </tr>
+                        <?php
+                          // get umum candidate information from DB
+                          foreach ($pilihanumum as $key => $candidate_id) {
+                            $query="SELECT c.*,v.*
+                                    FROM candidate as c 
+                                    JOIN voter as v
+                                    ON c.voter_id=v.voter_id 
+                                    WHERE c.candidate_id='$candidate_id'";
+                            $qr=mysqli_query($db,$query);
+                            if ($qr==false) {
+                                echo "Query cannot been executed<br>";
+                                echo "SQL error :".mysqli_error($db);
+                            }
+                            $record=mysqli_fetch_array($qr);
+                            $name=$record['voter_name'];
+                        ?> 
+                        <tr>
+                          <td><input name="umum<?=$key?>" type="text" readonly class="form-control-plaintext form-control-sm" value="<?=$candidate_id?>"></td>
+                          <td><?=$name?></td>
+                           <td><button class="btn btn-info btn-circle btn-sm "><i class="fas fa-info-circle"></i></button> </td>
 
+                        </tr>
+                        <!-- close loop -->
+                        <?php } ?>
+                      </table>
+                </div>
+                      <div class="col">
+                        <h3>candidate fakulti</h3>
+                        <table class="table table-bordered">
+                          <tr>
+                            <th class="th-sm" >Candidate ID</th>
+                            <th class="th-sm" >Name</th>
+                            <th class="th-sm" >Action</th>
 
-          <form method="POST" action="insertvotingintoDB.php">
-            <table class="table">
+                          </tr>
+                          <?php
+                            // get fakulti candidate information from DB
+                            foreach ($pilihanfakulti as $key => $candidate_id) {
+                              $query="SELECT c.*,v.*,f.*
+                                      FROM candidate as c 
+                                      JOIN voter as v
+                                      ON c.voter_id=v.voter_id 
+                                      JOIN faculty as f
+                                      ON v.faculty=f.faculty_id
+                                      WHERE c.candidate_id='$candidate_id'";
+                              $qr=mysqli_query($db,$query);
+                              if ($qr==false) {
+                                  echo "Query cannot been executed<br>";
+                                  echo "SQL error :".mysqli_error($db);
+                              }
+                              $record=mysqli_fetch_array($qr);
+                              $name=$record['voter_name'];
+                          ?> 
+                          <tr>
+                            <td><input name="<?=$record['name']?><?=$key?>" type="text" readonly class="form-control-plaintext form-control-sm" value="<?=$candidate_id?>"></td>            
+                            <td><?=$name?></td>
+                            <td><button class="btn btn-info btn-circle btn-sm "><i class="fas fa-info-circle"></i></button> </td>
 
-                <tr>
-                    <th class="th-sm" >Candidate ID</th>
-                    <th class="th-sm" >Name</th>
-                    <th class="th-sm" >input</th>
-                </tr>
-                <?php
-                // get umum candidate information from DB
-                 foreach ($pilihanumum as $key => $candidate_id) {
-
-                    $query="SELECT c.*,v.*
-                            FROM candidate as c 
-                            JOIN voter as v
-                            ON c.voter_id=v.voter_id 
-                            WHERE c.candidate_id='$candidate_id'";
-                        $qr=mysqli_query($db,$query);
-                        if ($qr==false) {
-                            echo "Query cannot been executed<br>";
-                            echo "SQL error :".mysqli_error($db);
-                        }
-                        $record=mysqli_fetch_array($qr);
-                        $name=$record['voter_name'];
-                ?> 
-                <tr>
-                         <td><?=$candidate_id?></td>
-                         <td><?=$name?></td>
-                         <td><input name="umum<?=$key?>" type="text" value="<?=$candidate_id?>"></td>
-                </tr>
-                       <?php }
-                       ?>
-                  </table>
-                  <h3>candidate fakulti</h3>
-                  <table class="table">
-                <tr>
-                    <th>Candidate ID</th>
-                    <th>Name</th>
-                    <th>input</th>
-                </tr>
-                <?php
-                // get fakulti candidate information from DB
-                 foreach ($pilihanfakulti as $key => $candidate_id) {
-                    $query="SELECT c.*,v.*,f.*
-                            FROM candidate as c 
-                            JOIN voter as v
-                            ON c.voter_id=v.voter_id 
-                            JOIN faculty as f
-                            ON v.faculty=f.faculty_id
-                            WHERE c.candidate_id='$candidate_id'";
-                    $qr=mysqli_query($db,$query);
-                    if ($qr==false) {
-                        echo "Query cannot been executed<br>";
-                        echo "SQL error :".mysqli_error($db);
-                    }
-                    $record=mysqli_fetch_array($qr);
-                    $name=$record['voter_name'];
-                       
-                ?> 
-                <tr>
-                  <td><?=$candidate_id?></td>            
-                  <td><?=$name?></td>
-                  <td><input name="<?=$record['name']?><?=$key?>" type="text" value="<?=$candidate_id?>"></td>
-                </tr>
-                <?php }?>
-
-            </table>
-          <input class="btn btn-primary " name="submit" type="submit"value="Submit">
-          </form>
-
+                          </tr>
+                          <!-- close loop -->
+                          <?php }?>
+                        </table>
+                      </div>
+                </div>
+                 <input class="btn btn-danger " name="btn_back" type="submit"value="Back">
+                 <input class="btn btn-primary " name="submit" type="button" value="Submit" onclick="history.back()" >
+                    </form>
+              </div>
             </div>
           </div>
         </div>
