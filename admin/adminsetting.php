1f013123f5e7997ddc23b0115979d7a527840cb4
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-07-14 21:56:28 +0800

    add mini navbar
    tambah mini navbar untuk asingkan general settting dengan section setting

--- admin/adminsetting.php
+++ admin/adminsetting.php
@@ -54,7 +54,6 @@
     }
     else
 
-
 // to change string to date format (start time)
 $start = strtotime($electiondetail['start']); 
 $starttime= date("Y-m-d\\Th:i:s", $start); 
@@ -66,15 +65,77 @@ include "include/header.template.php";
 ?>
 <div class="container-fluid">
 
-    <div class="card o-hidden border-0 shadow-lg my-1"  >
-    	<div class="card-header py-3" >
-          <h5 class="m-0  font-weight-bold text-primary">Settings</h5>
+     <ul class="nav nav-pills">
+              <li class="nav-item">
+                <a class="nav-link <?=$all_status?> " href="adminsetting.php">General </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link <?=$fstm_status?>" href="adminsetting.php?section=1">FSTM</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link <?=$fpm_status?> " href="adminsetting.php?section=3">FPM</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link <?=$fppi_status?> " href="adminsetting.php?section=4">FPPI</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link <?=$fsu_status?> " href="adminsetting.php?section=2">FSU</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link <?=$fp_status?> " href="adminsetting.php?section=5">FP</a>
+              </li>
+            </ul>
+            <hr>   
+    <?php 
+    if (isset($_GET['section'])) {
+         $data_id=$_GET['section'];
+         $get_ttl_candidate=mysqli_query($db,"SELECT s.section_name, count(c.candidate_id)as ttl from candidate as c JOIN section as s ON c.section_id=s.section_id WHERE c.section_id='$data_id'");
+        $ttl_candidate=mysqli_fetch_array($get_ttl_candidate);
+    ?>
+    <form class="user" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+        <div class="card o-hidden border-0 shadow-lg my-1"  >
+            <div class="card-header py-3" >
+                <h5 class="m-0  font-weight-bold text-primary"><?=$ttl_candidate['section_name']?> setting</h5>
+            </div>
+            <div class="card-body p-0" >
+             <!-- card padding -->
+                <div class="p-4">
+   
+
+                        <div class='form-group col-md-4'>
+                            <label>Section Instrution</label>
+                            <input name="txt_<?=$ttl_candidate['section_name']?>_instrution" type="text"  class="form-control " id="title" placeholder="Please select " value="">
+                            <label><?=$ttl_candidate['section_name']?> Max vote (total candidate:<?=$ttl_candidate['ttl']?>) </label>
+                            <select name=''  class='form-control '>
+                                <?php 
+                                    for($i=1;$i<=$ttl_candidate['ttl'];$i++){
+                                        echo " <option value='".$i."' >".$i."</option>";}
+                                 ?>
+                            </select>
+                        </div>
+                        <div align="right">
+                            <div  class="col-md-2">
+                                <input class="btn btn-primary btn-user btn-block" type="submit" name="btn_<?=$ttl_candidate['section_name']?>_setting" value="Save">
+                            </div>
+                        </div>
+                    
+                </div>
+            </div>
+        </div>
+    </form>
+    <?php }
+    else{
+      ?>
+       <!-- general setting card -->
+       <div class="card o-hidden border-0 shadow-lg my-1"  >
+        <div class="card-header py-3" >
+          <h5 class="m-0  font-weight-bold text-primary">General Settings</h5>
         </div>
       <div class="card-body p-0" >
          <!-- card padding -->
         <div class="p-4">
            <!-- form start -->
-    		  <form class="user" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+              <form class="user" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
 
              <!-- title textbox -->
              <div class="form-row">
@@ -85,20 +146,20 @@ include "include/header.template.php";
              </div>
              <!-- start date textbox -->
              <div class="form-row">
-   	           <div class="form-group col-md-4">
-     	 	          <label for="starttime">Election Start</label>
-     	 	          <input name="txt_start" type="datetime-local" class="form-control form-control-user" id="starttime" value="<?=$starttime?>">
-   	 		   	   </div>
-         	 		 <!-- end date textbox -->
-         			 <div class="form-group col-md-4">
-         				 <label for="endtime">Election End</label>
-         				 <input name="txt_end" type="datetime-local" class="form-control form-control-user" id="endtimes" value="<?=$endtime?>">
-         			 </div>
-   			     </div>
+               <div class="form-group col-md-4">
+                      <label for="starttime">Election Start</label>
+                      <input name="txt_start" type="datetime-local" class="form-control form-control-user" id="starttime" value="<?=$starttime?>">
+                   </div>
+                     <!-- end date textbox -->
+                     <div class="form-group col-md-4">
+                         <label for="endtime">Election End</label>
+                         <input name="txt_end" type="datetime-local" class="form-control form-control-user" id="endtimes" value="<?=$endtime?>">
+                     </div>
+                 </div>
                  <hr>
                  
-         		 <div align="right">
-         				<div  class="col-md-2">
+                 <div align="right">
+                        <div  class="col-md-2">
                     <input class="btn btn-primary btn-user btn-block" type="submit" name="btn_general_setting" value="Save">
                 </div>
              </div>
@@ -109,54 +170,9 @@ include "include/header.template.php";
       </div>
     <!-- close card element-->
     </div>
-    <hr>
-    <!-- second card -->
-    <div class="card o-hidden border-0 shadow-lg my-1"  >
-        <div class="card-header py-3" >
-          <h5 class="m-0  font-weight-bold text-primary">Section setting</h5>
-        </div>
-      <div class="card-body p-0" >
-         <!-- card padding -->
-        <div class="p-4">
-           <!-- form start -->
-              <form class="user" method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
-
-                     <div class="row">
-                        <?php 
-                             while ($section_detail=mysqli_fetch_array($qr)) {
-                                // get total candaite by faculty
-                                $section_id=$section_detail['section_id'];
-                                $get_ttl_candidate=mysqli_query($db,"SELECT count(candidate_id)as ttl from candidate WHERE section_id='$section_id'");
-                                $ttl_candidate=mysqli_fetch_array($get_ttl_candidate);
-
-                                    // dropbox for max vote
-                                    echo "<div class='form-group col-md-4'>
-                                    <label for='inputState'>".$section_detail['section_name']." Max vote (total candidate: ".$ttl_candidate['ttl']. ") </label>
-                                    <select name='".$section_detail['section_name']."' id='inputState' class='form-control '>";
-                                    for($i=1;$i<=$ttl_candidate['ttl'];$i++){
-                                        echo "<option value='".$i."' >".$i."</option>";
-                                    }
-                                        echo "</select>
-                                     </div>";
-                             // close while
-                             }
-                        ?>
-
-                    </div>
-                 
-                 <div align="right">
-                        <div  class="col-md-2">
-                    <input class="btn btn-primary btn-user btn-block" type="submit" name="btn_section_setting" value="Save">
-                </div>
-             </div>
-
-          </form>
-        </div>  
-      <!-- close card padding -->
-      </div>
-    <!-- close card element-->
-    </div>
-
+    <?php 
+    } ?>
+    
 <!-- close container-fluid -->
 </div>
 
