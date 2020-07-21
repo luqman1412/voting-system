4f3f1ee5c7b1ce333620885dd4c8d6cabbdd1e62
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-07-21 17:27:02 +0800

    add alreadyvote table
    add voter  to already vote table

--- voter/insertvotingintoDB.php
+++ voter/insertvotingintoDB.php
@@ -1,21 +1,24 @@
-<?php
+<?php session_start();
+$voter_id= $_SESSION['id'];
 include "../connection.php";
 
- foreach ($_POST as $arrayindex => $candidateid) {
-     $select['post'][$arrayindex] = $candidateid;
-     $insert= $select['post'][$arrayindex];
-    // check if the value is number
-     if (is_numeric($candidateid)==true) {
-        $query="UPDATE count SET total_vote=total_vote+1 WHERE candidate_id='$candidateid' ";
-        $qr=mysqli_query($db,$query);
-        if ($qr==false) {
-            echo "Query cannot been executed<br>";
-            echo "SQL error :".mysqli_error($db);
-        }
-    }
-    else
-    echo "vote succesfully submit";   
-
-    } 
-
+foreach ($_POST as $arrayindex => $candidateid) {
+    $select['post'][$arrayindex] = $candidateid;
+    $insert= $select['post'][$arrayindex];
+   // check if the candidate is number if true update count in DB if not throw error
+   if (is_numeric($candidateid)==true) {
+       $add_vote=mysqli_query($db,"UPDATE count SET total_vote=total_vote+1 WHERE candidate_id='$candidateid' ");
+       if ($add_vote==false) {
+           echo "Failed to add vote <br>";
+           echo "SQL error :".mysqli_error($db);
+       }
+       else
+           echo "vote succesfully submit<br>"; 
+   }
+   else
+       echo "invalid candiate id<br>"; 
+   } 
+// add voter to alreadyvote table
+$alreadyvote=mysqli_query($db,"INSERT INTO alreadyvote (voter_id) VALUES ('$voter_id')");
+echo " show successfull mesage ";
 ?>
