c90aa239db43c378fffcae2a11ab90c220975265
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-09-05 23:28:16 +0800

    review typo

--- alertfunction.php
+++ alertfunction.php
@@ -2,18 +2,18 @@
 
 function alertwithclose($message)
 {
-	echo '<div class="alert alert-danger" role="alert">'.$message.'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+	echo '<div class="alert alert-danger" role="alert"> '.$message.' <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 }
 function alert($message)
 {
-	echo '<div class="alert alert-danger" role="alert">'.$message.'</div>';
+	echo '<div class="alert alert-danger" role="alert"> '.$message.' </div>';
 }
 function successwithclose($message)
 {
-	echo '<div class="alert alert-success" role="alert">'.$message.'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+	echo '<div class="alert alert-success" role="alert"> '.$message.' <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 }
 function success($message)
 {
-	echo '<div class="alert alert-success" role="alert">'.$message.'</div>';
+	echo '<div class="alert alert-success" role="alert"> '.$message.' </div>';
 }
 ?>
